59744b59972db967495ae75d9233c703853a938e
elsokolova <sokelva@mail.ru>
2023-04-10 14:06:17 +0300

    Добавлены классы.

---
+++ SocialNetwork/DAL/Entities/FriendEntity.cs
@@ -0,0 +1,6 @@
+﻿public class FriendEntity
+{
+    public int id { get; set; }
+    public int user_id { get; set; }
+    public int friend_id { get; set; }
+}
---
+++ SocialNetwork/DAL/Entities/MessageEntity.cs
@@ -0,0 +1,7 @@
+﻿public class MessageEntity
+{
+    public int id { get; set; }
+    public string content { get; set; }
+    public int sender_id { get; set; }
+    public int recipient_id { get; set; }
+}
