e176deabc7ff91df7486b531395abcd38c93092b
elsokolova <sokelva@mail.ru>
2023-04-10 15:40:54 +0300

    Добавлен функционал в class Program.

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace SocialNetwork
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using System.Globalization;
+
+namespace SocialNetwork
 {
-    internal class Program
+    public class Program
     {
+        public static UserService userService = new UserService();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Добро пожаловать в социальную сеть!");
+
+            while (true)
+            {
+                Console.WriteLine("Для регистрации пользователя введите имя пользователя: ");
+                string firstname = Console.ReadLine();
+
+                Console.WriteLine("фамилия: ");
+                string lastname = Console.ReadLine();
+
+                Console.WriteLine("пароль: ");
+                string password = Console.ReadLine();
+
+                Console.WriteLine("почтовый адрес: ");
+                string email = Console.ReadLine();
+
+                UserRegistrationData registrationData = new UserRegistrationData()
+                {
+                    FirstName = firstname,
+                    LastName = lastname,
+                    Password = password,
+                    Email = email
+                };
+
+                try
+                {
+                    userService.Register(registrationData);
+                    Console.WriteLine("Регистрация проищошла успешно!");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Введите корректное значение.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Произошла ошибка при регистрации: {ex.Message}.");
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
