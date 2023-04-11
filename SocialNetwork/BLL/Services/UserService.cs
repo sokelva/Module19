8229baa4760b3ea39362958e596c0111ac06d357
elsokolova <sokelva@mail.ru>
2023-04-11 10:23:05 +0300

    Добавлены новые классы.

--- SocialNetwork/BLL/Services/UserService.cs
+++ SocialNetwork/BLL/Services/UserService.cs
@@ -1,26 +1,22 @@
-﻿using SocialNetwork.BLL.Models;
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL;
 using SocialNetwork.DAL.Repositories;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SocialNetwork.BLL.Services
 {
     public class UserService
     {
         IUserRepository userRepository;
-
-        public UserService() 
+        public UserService()
         {
             userRepository = new UserRepository();
-        }    
+        }
 
         public void Register(UserRegistrationData userRegistrationData)
         {
-
             if (String.IsNullOrEmpty(userRegistrationData.FirstName))
                 throw new ArgumentNullException();
 
@@ -33,25 +29,75 @@ namespace SocialNetwork.BLL.Services
             if (String.IsNullOrEmpty(userRegistrationData.Email))
                 throw new ArgumentNullException();
 
-            if (userRegistrationData.Password.Length <8 )
+            if (userRegistrationData.Password.Length < 8)
                 throw new ArgumentNullException();
 
-            if (new EmailAddressAttribute().IsValid(userRegistrationData.Email))
+            if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email))
                 throw new ArgumentNullException();
 
-            if (userRepository.FindByEmail(userRegistrationData.Email)!=null)
+            if (userRepository.FindByEmail(userRegistrationData.Email) != null)
                 throw new ArgumentNullException();
 
             var userEntity = new UserEntity()
             {
-                firstname= userRegistrationData.FirstName,
-                lastname= userRegistrationData.LastName,
-                email= userRegistrationData.Email,  
-                password= userRegistrationData.Password
+                firstname = userRegistrationData.FirstName,
+                lastname = userRegistrationData.LastName,
+                password = userRegistrationData.Password,
+                email = userRegistrationData.Email
             };
 
-            if(this.userRepository.Create(userEntity)==0)
+            if (this.userRepository.Create(userEntity) == 0)
                 throw new Exception();
+
+        }
+
+        public User Authenticate(UserAuthenticationData userAuthenticationData)
+        {
+            var findUserEntity = userRepository.FindByEmail(userAuthenticationData.Email);
+            if (findUserEntity is null) throw new UserNotFoundException();
+
+            if (findUserEntity.password != userAuthenticationData.Password)
+                throw new WrongPasswordException();
+
+            return ConstructUserModel(findUserEntity);
+        }
+
+        public User FindByEmail(string email)
+        {
+            var findUserEntity = userRepository.FindByEmail(email);
+            if (findUserEntity is null) throw new UserNotFoundException();
+
+            return ConstructUserModel(findUserEntity);
+        }
+
+        public void Update(User user)
+        {
+            var updatableUserEntity = new UserEntity()
+            {
+                id = user.Id,
+                firstname = user.FirstName,
+                lastname = user.LastName,
+                password = user.Password,
+                email = user.Email,
+                photo = user.Photo,
+                favorite_movie = user.FavoriteMovie,
+                favorite_book = user.FavoriteBook
+            };
+
+            if (this.userRepository.Update(updatableUserEntity) == 0)
+                throw new Exception();
+        }
+
+        private User ConstructUserModel(UserEntity userEntity)
+        {
+            return new User(userEntity.id,
+                          userEntity.firstname,
+                          userEntity.lastname,
+                          userEntity.password,
+                          userEntity.email,
+                          userEntity.photo,
+                          userEntity.favorite_movie,
+                          userEntity.favorite_book);
         }
     }
 }
